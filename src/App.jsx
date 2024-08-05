fa600e821b33107c7f59c6bb6aa8dfde0553899c
ShreyaLaheri <slaheri99@mail.com>
2024-08-04 21:35:09 -0400

    updated project structure

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import Header from './components/header/Header'
+import Navbar from './components/navbar/Navbar'
+import About from './components/about/About'
+import Experience from './components/experience/Experience'
+import Contact from './components/contact/Contact'
+import Footer from './components/footer/Footer'
+
+const App = () => {
+  return (
+    <>
+        <Header/>
+        <Navbar/>
+        <About/>
+        <Experience/>
+        <Contact/>
+        <Footer/>
+
+    </>
+  )
+}
+
+export default App
---
+++ src/components/contact/Contact.jsx
@@ -0,0 +1,11 @@
+import './contact.css';
+
+function Contact() {
+  return (
+    <div className="main-container">
+      Hi
+    </div>
+  );
+}
+
+export default Contact;
