849e22f307782e6681abcdded65770ea91915c4e
ShreyaLaheri <slaheri99@mail.com>
2025-01-02 19:32:10 -0500

    WIP added more details

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -2,8 +2,11 @@ import './about.css';
 
 function About() {
   return (
-    <div className="main-container">
-      Hi
+    <div className="about-container">
+		<h1>About Me</h1>
+		<p>Hi, I’m Shreya Laheri, a recent graduate from Drexel University with a focus on frontend engineering and web design. I’m passionate about creating beautiful and effective digital experiences that truly connect with people.
+
+		My goal is to use my creativity and technical skills to build scalable and impactful applications. I love turning ideas into engaging web solutions that make a difference..</p>
     </div>
   );
 }
--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -3,7 +3,9 @@ import './contact.css';
 function Contact() {
   return (
     <div className="main-container">
-      Hi
+		{/* refer micheal dangelo github */}
+    	<p>Feel free to get in touch. You can email me at:</p> 
+		<p>slaheri99@gmail.com</p>
     </div>
   );
 }
